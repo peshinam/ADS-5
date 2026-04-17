e79413176c2567c4d10eae950db9357c894934e1
peshinam <frrr.emaeeeee@mail.ru>
2026-04-17 23:21:54 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,42 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int N>
 class TStack {
-  // добавьте код стека
+private:
+    T arr[N];
+    int top;
+
+public:
+    TStack() {
+        top = -1;
+    }
+
+    void push(T value) {
+        if (top < N - 1) {
+            top++;
+            arr[top] = value;
+        }
+    }
+
+    T pop() {
+        if (top >= 0) {
+            top--;
+            return arr[top + 1];
+        }
+        return T();
+    }
+
+    T getTop() {
+        if (top >= 0) {
+            return arr[top];
+        }
+        return T();
+    }
+
+    bool isEmpty() {
+        return top == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
